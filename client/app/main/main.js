717c12b4ee3754de13012d7c9fd8c7a46e02c3d2
gemme <gernestome@hotmail.com>
2015-09-23 01:05:14 -0500

    ojala

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -8,5 +8,13 @@ angular.module('loopbackPassportAngularApp')
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('accesos', {
+        url: '/acceso',
+        templateUrl: 'app/acceso/acceso.html'
+      })
+      .state('fails', {
+        url: '/fail',
+        templateUrl: 'app/fail/fail.html'
       });
   });
